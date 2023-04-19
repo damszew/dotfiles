e2d5a1f212991172b266596e3beb80cc87b833b1
damszew <damian.szewczyk111@gmail.com>
2023-04-19 20:01:32 +0200

    Add onedark theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,14 @@
 return {
   { "Mofiqul/dracula.nvim" },
 
+  -- onedark looks better but not for rust :/ wait for better semantic support
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "deep",
+    },
+  },
+
   {
     "LazyVim/LazyVim",
     opts = {
